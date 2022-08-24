365b06369b95b9ba8aab8afc1179803511da69d9
DevTinel <davidaugustolol36@gmail.com>
2022-08-23 21:08:58 -0300

    fim

--- src/componentes/paginas/PaginaSobre.js
+++ src/componentes/paginas/PaginaSobre.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import styles from '../../css/PaginaSobre.module.css';
+import sobreImg from '../../assets/sobre-image.jpg';
+import sorveteria from '../../assets/sorveteria.jpg';
 
 export const PaginaSobre = () => {
   return (
@@ -12,7 +14,33 @@ export const PaginaSobre = () => {
           <h1>A GELATERIA</h1>
         </div>
 
-        <div></div>
+        <div className={styles.BoxSobreNos}>
+          <h2>Sobre Nós</h2>
+          <span>
+            <em>Nós simplesmente amamos sorvete </em>
+          </span>
+          <p>
+            Nós simplesmente amamos sorvete! Somos uma empresa apaixonada pelo
+            que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor
+            sorvete para os nossos clientes e gostamos de receber elogios.
+            Estamos operando desde 2009 com as melhores matérias-primas para a
+            produção final do sorvete. Vendemos tanto para varejo como para
+            atacado.
+          </p>
+          <p>
+            Nossos clientes podem comprar os nossos sorvetes e degustar na nossa
+            loja ou levar para sua residência e aproveitar junto com a família.
+            Também vendemos para estabelecimentos e criamos eventos como festa
+            de aniversário, formaturas e eventos empresariais. Para contratar os
+            nossos serviços, basta entrar em contato conosco. Iremos
+            proporcionar o melhor atendimento e os melhores produtos para você
+            fazer a sua festa mais saborosa, com o melhor sorvete da cidade.
+          </p>
+        </div>
+        <div className={styles.boxImages}>
+          <img src={sobreImg} alt="" />
+          <img src={sorveteria} alt="" />
+        </div>
       </section>
       <Footer />
     </>
